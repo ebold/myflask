1f6c9f04c1387c622f446dced43d4f9805b359b1
Enkhbold Ochirsuren <boldoodd@gmail.com>
2020-06-02 20:19:03 +0200

    poll: get voting

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
  
 poll_data = {
-   'question' : 'Сонголтоо хийнэ үү?',
-   'fields'   : ['С.Баяр', 'Х.Номтойбаяр', 'Н.Энхбаяр', 'Б.Эрдэнэбаяр', 'сонголт алга']
+   'question' : 'Та сонголтоо хийнэ үү?',
+   'fields'   : ['С.Баяр', 'Х.Номтойбаяр', 'Н.Энхбаяр', 'Б.Эрдэнэбаяр', 'үгүй']
 }
- 
+filename = 'data.txt'
+
 @app.route('/')
 def root():
     return render_template('poll.html', data=poll_data)
- 
+
+@app.route('/poll')
+def poll():
+    vote = request.args.get('field')
+
+    out = open(filename, 'a')
+    out.write( vote + '\n' )
+    out.close()
+
+    return render_template('thankyou.html', data=poll_data)
+
 if __name__ == "__main__":
     app.run(debug=True)
