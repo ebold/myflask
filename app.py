b074b12a7986b2b6a611f5de360f4350fb6834c0
Enkhbold Ochirsuren <boldoodd@gmail.com>
2020-06-02 20:02:08 +0200

    poll: added polling data

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 import os
 app = Flask(__name__)
  
+poll_data = {
+   'question' : 'Сонголтоо хийнэ үү?',
+   'fields'   : ['С.Баяр', 'Х.Номтойбаяр', 'Н.Энхбаяр', 'Б.Эрдэнэбаяр', 'сонголт алга']
+}
+ 
 @app.route('/')
 def root():
-    return render_template('poll.html')
+    return render_template('poll.html', data=poll_data)
  
 if __name__ == "__main__":
     app.run(debug=True)
